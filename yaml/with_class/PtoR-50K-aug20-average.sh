2f68cb43129f13936c00bc6db26b9b7eafe5d0bf
yyc776 <61278073+yyc776@users.noreply.github.com>
2023-02-26 13:28:37 +0800

    Add files via upload

---
+++ yaml/with_class/PtoR-50K-aug20-average.sh
@@ -0,0 +1,6 @@
+onmt_average_models -output  save_model/stage_two/with_class/aug20/average_model_56-60.pt \
+    -m  save_model/stage_two/with_class/aug20/model.product-reactants_step_560000.pt \
+        save_model/stage_two/with_class/aug20/model.product-reactants_step_570000.pt \
+        save_model/stage_two/with_class/aug20/model.product-reactants_step_580000.pt \
+        save_model/stage_two/with_class/aug20/model.product-reactants_step_590000.pt \
+        save_model/stage_two/with_class/aug20/model.product-reactants_step_600000.pt
